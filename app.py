3de7c910e454b2da4e8cb9cffe154c644ff38017
iamkorangi <ganeshkorangi87@gmail.com>
2026-07-31 15:32:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def home():
     })
 
 @app.route("/health")
-def health()
+def health():
 	return jsonify({"status": "healthy"}), 200
 
-if __name__== "__main__":
+if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000)
